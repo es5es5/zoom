b58fefd18893fdd8f2b24f517bd1c998a0468160
es5es5 <es5e5s@nate.com>
2022-12-02 18:55:16 +0900

    Nickname Form

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,15 @@
 const messageList = document.querySelector('ul')
-const messageForm = document.querySelector('form')
+const messageForm = document.querySelector('#message')
+const nicknameForm = document.querySelector('#nickname')
 
 const socket = new WebSocket(`ws://${window.location.host}`)
+
+function makeMessage(type, payload) {
+  return JSON.stringify({
+    type,
+    payload
+  })
+}
 
 socket.addEventListener('open', () => {
   console.log('Connected to Server.')
@@ -21,8 +29,16 @@ socket.addEventListener('close', () => {
 function handleSubmit(event) {
   event.preventDefault()
   const input = messageForm.querySelector('input')
-  socket.send(input.value)
+  socket.send(makeMessage('new_message', input.value))
+  input.value = ''
+}
+
+function handleNicknameSubmit(event) {
+  event.preventDefault()
+  const input = nicknameForm.querySelector('input')
+  socket.send(makeMessage('nickname', input.value))
   input.value = ''
 }
 
 messageForm.addEventListener('submit', handleSubmit)
+nicknameForm.addEventListener('submit', handleNicknameSubmit)
